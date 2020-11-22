7a611183015f89f6beeeb71161afa2eede7a83bd
Cyrille <cyrille@cbiot.fr>
2020-11-22 10:04:56 +0100

    Add about dial box. Test param before write file config

--- create_autostart_launcher.py
+++ create_autostart_launcher.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+pwgen.py
+Utilitaire de création de mot de passe
+Aucune utilité, juste pédagogique pou manipuler GTK
+
+  Source : https://github.com/CyrilleBiot/pwgen
+
+__author__ = "Cyrille BIOT <cyrille@cbiot.fr>"
+__copyright__ = "Copyleft"
+__credits__ = "Cyrille BIOT <cyrille@cbiot.fr>"
+__license__ = "GPL"
+__version__ = "2.0"
+__date__ = "2020/11/10"
+__maintainer__ = "Cyrille BIOT <cyrille@cbiot.fr>"
+__email__ = "cyrille@cbiot.fr"
+__status__ = "Devel"
+"""
+
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, GdkPixbuf
 import re # regex
 import os
 
@@ -81,13 +99,13 @@ class FileChooserWindow(Gtk.Window):
 
 
         # The Switch Button
-        switch = Gtk.Switch()
-        switch.connect("notify::active", self.on_switch_activated)
-        switch.set_active(False)
-        switch.set_vexpand(False)
-        switch.set_hexpand(False)
-        switch.set_halign(True)
-        switch.set_tooltip_text("Attention ! Soyez sûr des modification apportées manuellement !")
+        self.switch = Gtk.Switch()
+        self.switch.connect("notify::active", self.on_switch_activated)
+        self.switch.set_active(False)
+        self.switch.set_vexpand(False)
+        self.switch.set_hexpand(False)
+        self.switch.set_halign(True)
+        self.switch.set_tooltip_text("Attention ! Soyez sûr des modification apportées manuellement !")
 
         # Button Create starter
         btnCreateFile = Gtk.Button(label="Générer le fichier")
@@ -99,11 +117,12 @@ class FileChooserWindow(Gtk.Window):
 
         # Button About Dialog
         btnAbout = Gtk.Button(label="About")
+        btnAbout.connect("clicked", self.on_clic_about)
 
         grid = Gtk.Grid()
         grid.set_column_spacing(6)
         grid.set_row_spacing(6)
-        #grid.set_row_homogeneous(True)
+        grid.set_row_homogeneous(False)
         grid.attach(self.entryName, 0, 0, 1, 1)
         grid.attach(btnName, 1, 0, 2, 1)
         grid.attach(self.entryFile,0,1,1,1)
@@ -117,17 +136,63 @@ class FileChooserWindow(Gtk.Window):
         grid.attach(scrolled_window,0,4,1,10)
         grid.attach(self.img,1,5,2,1)
         grid.attach(labelSwitch,1,13,1,1)
-        grid.attach(switch,2,13,1,1)
+        grid.attach(self.switch,2,13,1,1)
         grid.attach(btnCreateFile, 0, 14, 2, 1)
         grid.attach(btnAbout, 2, 14, 1, 1)
 
         self.add(grid)
 
+    def warning_alert(self, widget, message1, message2):
+        dialog = Gtk.MessageDialog(
+            transient_for=self,
+            flags=0,
+            message_type=Gtk.MessageType.ERROR,
+            buttons=Gtk.ButtonsType.CANCEL,
+            text=message1,
+        )
+        dialog.format_secondary_text(message2)
+        dialog.run()
+
+        dialog.destroy()
+
     def create_autostart_file(self,widget):
-        homedir = os.environ['HOME']
-        name_of_file = homedir + '/.config/autostart/' + self.entryName.get_text() +'.desktop'
-        name_of_file = ''.join(name_of_file.split())
-        print(name_of_file)
+
+        # If edition mode, save the current buffer. Without any test (name, file, icon...)
+        if self.switch.get_active():
+            print('Mode édition')
+            start_iter = self.text_buffer.get_start_iter()
+            end_iter = self.text_buffer.get_end_iter()
+            self.long_text = self.text_buffer.get_text(start_iter, end_iter, True)
+        else:
+            print('EDITION NOT')
+
+            # Test of existence files to exec and icon
+            if not os.path.isfile(self.entryFile.get_text()):
+                print("Fichier FILE inexistant")
+                return
+
+            if not os.path.isfile(self.entryIcon.get_text()):
+                print("Fichier ICON inexistant")
+                return
+
+            # Test is the file.desktop already exists or not
+            homedir = os.environ['HOME']
+            name_of_file = homedir + '/.config/autostart/' + self.entryName.get_text() + '.desktop'
+            name_of_file = ''.join(name_of_file.split())
+            print(name_of_file)
+
+            if os.path.isfile(name_of_file):
+                print("Le fichier existe. On quitte.")
+                msg1 = "Un fichier portant ce nom existe déjà !"
+                msg2 = "Veuillez changer son nom. Ou effacer le fichier déjà existant."
+                self.warning_alert(self, msg1, msg2)
+                return
+            return
+
+        print(self.long_text) # DEBUG
+        return
+
+        # Record the configuration file to  .config/autostart/NameOfFile.desktop
         file = open(name_of_file, "x")
         file.write(self.long_text)
         file.close()
@@ -162,8 +227,6 @@ class FileChooserWindow(Gtk.Window):
             entryUpdate = self.entryIcon.get_text()
             self.long_text = re.sub("Icon=.*\n", 'Icon=' + entryUpdate + '\n', self.long_text)
             self.img.set_from_file(entryUpdate)
-
-
 
         self.text_buffer.set_text(self.long_text)
 
@@ -201,9 +264,8 @@ class FileChooserWindow(Gtk.Window):
         dialog.destroy()
 
     def add_filters(self, widget, dialog):
-
+        # Filters for the file
         if widget.get_label() == "File Selection":
-            print("FILE")
             filter_text = Gtk.FileFilter()
             filter_text.set_name("Text files")
             filter_text.add_mime_type("text/plain")
@@ -219,6 +281,7 @@ class FileChooserWindow(Gtk.Window):
             filter_any.add_pattern("*")
             dialog.add_filter(filter_any)
 
+        # Filters for the icon file
         elif widget.get_label() == "Icon Selection":
             print("ICON")
             filter_png = Gtk.FileFilter()
@@ -246,6 +309,63 @@ class FileChooserWindow(Gtk.Window):
             filter_any.add_pattern("*")
             dialog.add_filter(filter_any)
 
+    def on_clic_about(self, widget):
+        """
+        Fonction de la Boite de Dialogue About
+        :param widget:
+        :return:
+        """
+        # Recuperation n° de version
+        lignes = __doc__.split("\n")
+        for l in lignes:
+            if '__version__' in l:
+                version = l[15:-1]
+            if '__date__' in l:
+                dateGtKBox = l[12:-1]
+
+        authors = ["Cyrille BIOT"]
+        documenters = ["Cyrille BIOT"]
+        self.dialog = Gtk.AboutDialog()
+        logo = GdkPixbuf.Pixbuf.new_from_file("./apropos.png")
+        if logo != None:
+            self.dialog.set_logo(logo)
+        else:
+            print("A GdkPixbuf Error has occurred.")
+        self.dialog.set_name("Gtk.AboutDialog")
+        self.dialog.set_version(version)
+        self.dialog.set_copyright("(C) 2020 Cyrille BIOT")
+        self.dialog.set_comments("pwgen.py.\n\n" \
+                                 "[" + dateGtKBox + "]")
+        self.dialog.set_license("GNU General Public License (GPL), version 3.\n"
+                                "This program is free software: you can redistribute it and/or modify\n"
+                                "it under the terms of the GNU General Public License as published by\n"
+                                "the Free Software Foundation, either version 3 of the License, or\n"
+                                "(at your option) any later version.\n"
+                                "\n"
+                                "This program is distributed in the hope that it will be useful,\n"
+                                "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+                                "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+                                "GNU General Public License for more details.\n"
+                                "You should have received a copy of the GNU General Public License\n"
+                                "along with this program.  If not, see <https://www.gnu.org/licenses/>\n")
+        self.dialog.set_website("https://cbiot.fr")
+        self.dialog.set_website_label("cbiot.fr")
+        self.dialog.set_website("https://github.com/CyrilleBiot/pwgen")
+        self.dialog.set_website_label("GIT pwgen")
+        self.dialog.set_authors(authors)
+        self.dialog.set_documenters(documenters)
+        self.dialog.set_translator_credits("Cyrille BIOT")
+        self.dialog.connect("response", self.on_click_response_about)
+        self.dialog.run()
+
+    def on_click_response_about(self, widget, response):
+        """
+        Fonction fermant la boite de dialogue About
+        :param widget:
+        :param response:
+        :return:
+        """
+        self.dialog.destroy()
 
 win = FileChooserWindow()
 win.connect("destroy", Gtk.main_quit)
